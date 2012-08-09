ee885d8207eafb13c011ac8e9bb5f72c4cb8eb56
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-08-09 22:37:27 +0700

    imager config example

---
+++ imager-example.js
@@ -0,0 +1,30 @@
+module.exports = {
+  variants: {
+    items: {
+      resize: {
+        mini : "300x200",
+        preview: "800x600"
+      },
+      crop: {
+        thumb: "200x200"
+      }
+    },
+
+    gallery: {
+      crop: {
+        thumb: "100x100"
+      }
+    }
+  },
+
+  storage: {
+    Rackspace: {
+      auth: {
+        username: "USERNAME",
+        apiKey: "API_KEY",
+        host: "lon.auth.api.rackspacecloud.com"
+      },
+      container: "CONTAINER_NAME"
+    }
+  }
+}

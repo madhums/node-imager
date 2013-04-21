0750d01ed447d5304bf0722464b7b78bc7222281
Madhusudhan Srinivasa <madhums8@gmail.com>
2013-04-22 01:53:25 +0530

    add tests and refactor the lib for better error handling and flexible syntax

--- test/test.js
+++ test/test.js
@@ -1,41 +1,94 @@
-var Imager = require('../index')
+
+/**
+ * Module dependencies.
+ */
+
+var Imager = require('../')
   , should = require('should')
   , imagerConfig = require('./imager')
-  , imager = new Imager(imagerConfig, 'Rackspace')
-  , filesToRemove
 
-describe('Imager', function () {
-  describe('Single image from disk', function () {
-    it('should upload to rackspace', function (done) {
-      imager = new Imager(imagerConfig, 'Rackspace')
-      imager.upload(__dirname+'/fixtures/single.jpg', function(err, cdnUri, files){
-        files.should.be.a('object')
-        files.length.should.be.equal(1)
-        done()
-      }, 'items')
-    })
+describe('new Imager()', function () {
+  it('should throw if no parameters are passed', function () {
+    (function () {
+      var imager = new Imager()
+    }).should.throw('Please provide the config')
   })
 
-  describe('Multiple images from disk', function () {
-    it('should upload to rackspace', function (done) {
-      var arr = [__dirname+'/fixtures/single.jpg', __dirname+'/fixtures/multiple-3.png']
-      imager = new Imager(imagerConfig, 'Rackspace')
-      imager.upload(arr, function(err, cdnUri, files){
-        files.should.be.a('object')
-        files.length.should.be.equal(2)
-        filesToRemove = files
-        done()
-      }, 'items')
-    })
+  it('should throw if no config is provided', function () {
+    (function () {
+      var imager = new Imager('Rackspace')
+    }).should.throw('Please provide the config')
   })
 
-  describe('Remove images', function () {
-    it('should remove the images from Rackspace', function (done) {
-      imager = new Imager(imagerConfig, 'Rackspace')
-      imager.remove(filesToRemove, function(err){
-        should.not.exist(err)
-        done()
-      }, 'items')
-    })
+  it('should throw if config is null or undefined', function () {
+    (function () {
+      var imager = new Imager(null, 'Rackspace')
+    }).should.throw('Please provide the config')
+  })
+
+  it('should throw if config doesnt have storage', function () {
+    (function () {
+      var imager = new Imager({}, 'Rackspace')
+    }).should.throw('Please specify a storage')
+  })
+
+  it('given proper config - should create the instance', function () {
+    var imager = new Imager(imagerConfig, 'Rackspace')
+
+    imager.config.should.have.property('variants')
+    imager.config.should.have.property('storage')
+    should.strictEqual('Rackspace', imager.storage)
+    imager.uploadedFiles.should.be.empty
   })
 })
+
+describe('Imager', function () {
+  describe('#upload()', function () {
+    it('should throw if no variant is provided', function () {
+      (function () {
+        var imager = new Imager(imagerConfig, 'Rackspace')
+        imager.upload()
+      }).should.throw('Please specify a proper variant OR provide a default')
+    })
+
+    it('should throw if the variant is not in the config', function () {
+      (function () {
+        var imager = new Imager(imagerConfig, 'Rackspace')
+        imager.upload([], 'abc')
+      }).should.throw('Please provide a variant which you have specified in the config file')
+    })
+
+    it('should do nothing if the files are not provided', function (done) {
+      var imager = new Imager(imagerConfig, 'Rackspace')
+      imager.upload([], function (err, cdnUri, files) {
+        should.not.exist(err)
+        should.not.exist(cdnUri)
+        files.should.be.empty
+        done()
+      }, 'items')
+    })
+
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

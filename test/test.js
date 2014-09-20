8cd85daed57a125c668308e8e122e794eb88c5a4
Madhusudhan Srinivasa <madhums8@gmail.com>
2014-09-20 13:50:44 +0200

    add tests for uploading without presets

--- test/test.js
+++ test/test.js
@@ -3,8 +3,10 @@
  * Module dependencies.
  */
 
+var fs = require('fs');
 var Imager = require('..');
 var should = require('should');
+var request = require('superagent');
 var co = require('co');
 var config = require('./imager');
 var files = [
@@ -39,6 +41,36 @@ describe('imager.upload', function () {
         'https://'+ config.storages.amazon.container +'.s3.amazonaws.com/user/1/thumb/image-1.png',
         'https://'+ config.storages.amazon.container +'.s3.amazonaws.com/user/1/thumb/image-2.jpg'
       ])
+      done(err);
+    });
+  });
+
+  // Figure out a way to test the original image was uploaded
+  it.skip('should obey preset.original option and upload original image without processing', function (done) {
+    var stat = fs.statSync(files[1]);
+    delete config.variants.item.thumb;
+    var imager = new Imager(config.variants.item, config.storages.rackspace);
+    imager.upload(files[1], function (err, uploaded) {
+      should.not.exist(err);
+      should.exist(uploaded.large[0]);
+      // uploaded.large[0].should.equal('https://'+ config.storages.amazon.container +'.s3.amazonaws.com/large_image-2.jpg');
+      request
+        .get(uploaded.large[0])
+        .end(function (err, res) {
+          res.headers['content-length'].should.equal(stat.size);
+          done(err);
+        });
+    });
+  });
+
+  it('should upload the files even when presets are not provided', function (done) {
+    var imager = new Imager(config.storages.amazon);
+    imager.upload(files, function (err, uploaded) {
+      should.not.exist(err);
+      uploaded.original.should.containDeep([
+        'https://'+ config.storages.amazon.container +'.s3.amazonaws.com/original_image-1.png',
+        'https://'+ config.storages.amazon.container +'.s3.amazonaws.com/original_image-2.jpg'
+      ]);
       done(err);
     });
   });
